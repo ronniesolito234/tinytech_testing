6c9341ff6da2b87fa73fac61c660b35094e68186
ronniesolito234 <ronniesolito234>
2026-06-01 23:02:25 +0800

    Added Pool Id, ClientID, and Region

--- js/config.js
+++ js/config.js
@@ -1,9 +1,9 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
+        userPoolId: '28oq21s5icibuukdu4t0d2ofda', 
+        userPoolClientId: 'ap-southeast-1_VotqtNkza', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-1' // e.g. us-east-2
+    },  
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
