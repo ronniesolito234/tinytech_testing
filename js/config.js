8250f8985782b4be2121142a2f286c0255697338
ronniesolito234 <ronniesolito234@gmail.com>
2026-06-17 09:25:18 +0800

    Testing Commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-southeast-1_9DgPWjkIh', 
+        userPoolClientId: '6gaj2mi83t6gkiqijqprccbsoh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-1' // e.g. us-east-2
+    },  
+    api: {
+        invokeUrl: 'https://n0d8g3fts0.execute-api.ap-southeast-1.amazonaws.com/Dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
